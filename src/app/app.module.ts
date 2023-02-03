25913562f9502081bba60cb78aca329efcce9863
Luiz <luiz@MacBook-Air-de-Luiz.local>
2023-02-03 16:53:26 -0300

    feat: init project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,28 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {PrettyJsonPipe} from "./pipes/prettyjson.pipe";
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {AngJsoneditorModule} from "@maaxgr/ang-jsoneditor";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PrettyJsonPipe,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    AngJsoneditorModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
