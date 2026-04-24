aec83c9547c3c1f4960d91f3d7a2956542c1a78e
Luiz Carlos <rosestolato@paytime.com.br>
2026-04-23 21:05:36 -0300

    fix: envs

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://0.0.0.0:3000',
+  apiUrl: 'https://api.jsonedit.com.br',
   socketUrl: 'wss://api.jsonedit.com.br:8002/items'
 };
